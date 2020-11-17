df98fe6765b817cddeb204bfffbca0f04ef8d608
anthonyhorgan <ahorgan321@gmail.com>
2020-11-17 18:07:42 +0000

    created required classes

---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name;
+    private int age;
+    //TODO change dob to date
+    private String dob;
+    private int id;
+    private Course course;
+
+    public Student(){
+
+    }
+}
